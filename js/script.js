466391578908851759ef337aeedfb50db5467aec
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-03-19 18:40:03 +0000

    Updated web page to calculate area and perimeter of a rectangle

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-function doMathClicked () {
-  // this function does basic math
-  document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
-  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
-  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
-	document.getElementById('square-root-math').innerHTML = '√(81) = ' + Math.sqrt(81)
+function calculateArea () {
+  // this function calculates area
+  document.getElementById('multiply-math').innerHTML = 'The area is ' + (5 * 3) + ' cm²'
+}
+
+function calculatePerimeter () {
+  // this function calculates perimeter
+  document.getElementById('add-math').innerHTML = 'The perimeter is ' + (5 * 2 + 3 * 2) + ' cm'
 }
